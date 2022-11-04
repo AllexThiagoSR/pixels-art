42f555ce77538583edb2047f7439bed190dd22bb
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-04 16:51:11 -0300

    Finaliza os requisitos 3 e 4

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const buttonToRandom = document.getElementById('button-random-color');
 const colors = document.getElementsByClassName('color');
 
 const randomizeColor = () => {
-  const red = Math.ceil(Math.random() * 255);
-  const green = Math.ceil(Math.random() * 255);
-  const blue = Math.ceil(Math.random() * 255);
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
 
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
-buttonToRandom.addEventListener('click', () => {
+const chooseColors = () => {
   for (let color of colors) {
     if (!color.className.includes('black')){
+
       color.style.backgroundColor = randomizeColor();
     }
   }
-})
+}
+
+buttonToRandom.addEventListener('click', chooseColors);
+
+window.onload = chooseColors;
