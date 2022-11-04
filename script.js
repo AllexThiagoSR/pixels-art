988c011851b52823c99efd4d397990670094ebed
Allex Thiago <allexthiagogatinho502@gmail.com>
2022-11-04 16:14:34 -0300

    Cria função que retorna uma string com uma cor aleatória

---
+++ script.js
@@ -0,0 +1,16 @@
+const buttonToRandom = document.getElementById('button-random-color');
+const colors = document.getElementsByClassName('color');
+
+const randomizeColor = () => {
+  const red = Math.ceil(Math.random() * 255);
+  const green = Math.ceil(Math.random() * 255);
+  const blue = Math.ceil(Math.random() * 255);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+};
+
+buttonToRandom.addEventListener('click', () => {
+  for (let color of colors) {
+    console.log(randomizeColor());
+  }
+})
